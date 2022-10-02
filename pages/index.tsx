80e77eda0899a4977edc6494ab155479d2221d5c
MadMan24 <mdverdera@yahoo.com>
2022-10-02 22:54:00 +0800

    Productive section.

--- pages/index.tsx
+++ pages/index.tsx
@@ -133,6 +133,57 @@ const Home: NextPage = () => {
           </div>
         </div>
       </section>
+
+      {/* Productive Section */}
+      <section id="productive" className="bg-white dark:bg-darkBlue">
+        {/* Productive Container */}
+        <div className="container flex flex-col items-center mx-auto px-6 pt-24 pb-32 md:flex-row md:space-x-16">
+          {/* Image */}
+          <div className="md:w-1/2">
+            <picture>
+              <img
+                src="/img/illustration-stay-productive.png"
+                alt="stay productive"
+                className="mb-10"
+              />
+            </picture>
+          </div>
+          {/* Content */}
+          <div className="flex flex-col items-start md:w-1/2">
+            <div className="flex flex-col space-y-5">
+              <h4 className="max-w-md text-xl font-bold md:text-4xl">
+                Stay productive, wherever you are
+              </h4>
+              <p className="text-md md:text-lg">
+                Never let location be an issue when accessing your files. Fylo
+                has you covered for all of your file storage needs.
+              </p>
+
+              <p className="text-md md:text-lg">
+                Securely share files and folders with friends, family and
+                colleagues for live collaboration. No email attachments
+                required.
+              </p>
+            </div>
+
+            {/* Link */}
+            <div className="block mt-4">
+              <Link href="#">
+                <a className="border-b border-accentCyan text-accentCyan">
+                  See how Flyo works
+                  <picture>
+                    <img
+                      src="/img/icon-arrow.svg"
+                      alt="arrow"
+                      className="inline pb-2 pl-1"
+                    />
+                  </picture>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
